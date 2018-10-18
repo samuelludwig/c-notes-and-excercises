b6fa77b18cf4ecd1055c64ab73327388d0029600
samuelludwig <samuelludwig74@yahoo.com>
2018-10-18 09:05:47 -0400

    began working on foundation of next few functions, cleaned up some code (thanks DRY), updated reqs list with progress

--- cross_ref.c
+++ cross_ref.c
@@ -6,13 +6,23 @@
 
 #define MAXSIZE 1000
 void copy(char to[], char from[]);
-void define_noise(char string[MAXSIZE][MAXSIZE]);
+void define_noise(char string[][MAXSIZE]);
+char *build_word(char initialchar);
+bool is_noise(char string[], char noise_array[][MAXSIZE]);
+
+typedef struct Word {
+    char this_word[MAXSIZE];    // a chararray (string)- the word to be tracked 
+    int lines_found[MAXSIZE];  // an array of integers indicating lines found
+    struct word *next_word;
+} Word;
 
 int main(int argc, char const *argv[])
 {
     char *word_array[MAXSIZE];
     char noise_words[MAXSIZE][MAXSIZE];
     define_noise(noise_words);
+
+    Word word_bank[MAXSIZE];
     return 0;
 }
 
@@ -43,7 +53,7 @@ int main(int argc, char const *argv[])
 //     }
 // }
 
-void define_noise(char string[MAXSIZE][MAXSIZE])
+void define_noise(char string[][MAXSIZE])
 {
     printf("Define noise words, capitalization is irrelevant (input as alphanumeric words separated by blanks):\n");
     
@@ -51,54 +61,58 @@ void define_noise(char string[MAXSIZE][MAXSIZE])
     int wordcounter = 0;
     while (c != EOF) {
         if (isalnum(c)) {
-            char word[MAXSIZE];
-            for (int i = 0; isalnum(c); i++) {
-                word[i] = c;
-                c = getchar();
-                if (!isalnum(c)) {
-                    word[i+1] = '\0';
-                }
-            }
-            //
-            // then add word to string[] of noise words
+            char *word = build_word(c);
+
             int i;
             for (i = 0; word[i] != '\0'; i++){
                 string[wordcounter][i] = word[i];
             }
             string[wordcounter][i+1] = '\0';
             wordcounter++;
-            //
         } else {
             c = getchar();
         }
     }
 
-    printf("first word banned is : %s\n", *&string[0]);
-    printf("word count: %d", wordcounter);
+    printf("Restrictions defined...\n");
 }
 
 void traverse_file()
 {
+    int linenum = 0;
 
+    int c = ' ';
+    while(c != EOF){
+        if (c == '\n') {
+            linenum++;
+        }
+
+        
+        if (isalnum(c)) {
+            char *word = build_word(c);
+        }    
+    }
 }
 
-// bool is_noise(char *string, char *noise_words[])
-// {
-//     for (int i = 0; i < sizeof(noise_words); i++) {
-//         if (strcmp(*string, *noise_words[i])) {
-//             return true;
-//         }
-//     }
-
-//     return false;
-// }
-
-void to_char_array(char input[]) 
+bool is_noise(char *string, char noise_words[][MAXSIZE])
 {
-    
+    size_t noise_size = (sizeof(noise_words)/sizeof(noise_words[0][0]));
+    for (int i = 0; i < noise_size; i++) {
+        
+    }
+
+    return false;
 }
 
-// struct word {
-//     char this_word[MAXSIZE];    // a chararray (string)- the word to be tracked 
-//     int lines_found[];  // an array of integers indicating lines found
-// }
+char *build_word(char initialchar) 
+{
+    char word[MAXSIZE];
+    for (int i = 0; isalnum(initialchar); i++) {
+        word[i] = initialchar;
+        initialchar = getchar();
+        if (!isalnum(initialchar)) {
+            word[i+1] = '\0';
+        }
+    }
+    return word;
+}
--- cross_ref_reqs.md
+++ cross_ref_reqs.md
@@ -1,7 +1,6 @@
 ## Goal
 
-- Write a cross referencer that prints a lits of all words in a document, 
-and, for each word a list of the line numbers on which it occurs.
+- Write a cross referencer that prints a list of all words in a document, and, for each word a list of the line numbers on which it occurs.
 
 - Remove noise words like "the", "and", etc.
 
@@ -9,18 +8,19 @@ and, for each word a list of the line numbers on which it occurs.
 
 ## Routines/items needed:
 
-- function that prompts the user to define what will be considered "noise words"
+- function that prompts the user to define what will be considered "noise words"    **[DONE]**
+    - array to put the noise words into **[DONE]**
 
 - loop through file comparing each word to the noise list, if not, update 'lines found'
 
 - check if noise
     - for each word, will loop through predefined array of 'noise words' and ignore if found in the array
 
-- line number incrementer for every '\n'
+- line number incrementer for every '\n' **[DONE]**
 
 - word struct with properties (the_word, appeared_lines)
-    - appeared_lines will be a pointer to an array of ints
+    - appeared_lines will be a pointer to an array of ints **[DONE]**
 
 - array to store pointers to every word object
-    - array will be sorted alphabetically 
+    - array will be sorted alphabetically by word property
                 - array will be a binary tree
